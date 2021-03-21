f9749a74d833b419f4e42df5a0f7e5fa3d10da09
blaire <blaire.e.porter@gmail.com>
2021-03-21 17:38:54 -0400

    jquery form

--- js/script.js
+++ js/script.js
@@ -47,3 +47,36 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+
+window.formbutton = window.formbutton || function () { (formbutton.q = formbutton.q || []).push(arguments) };
+
+/* customize formbutton below*/
+formbutton("create", {
+    action: "https://formspree.io/f/moqyajdl",
+    title: "How can we help?",
+    fields: [
+        {
+            type: "email",
+            label: "Email:",
+            name: "email",
+            required: true,
+            placeholder: "your@email.com"
+        },
+        {
+            type: "textarea",
+            label: "Message:",
+            name: "message",
+            placeholder: "What's on your mind?",
+        },
+        { type: "submit" }
+    ],
+    styles: {
+        title: {
+            backgroundColor: "gray"
+        },
+        button: {
+            backgroundColor: "gray"
+        }
+    }
+});
