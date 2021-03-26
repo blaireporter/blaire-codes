fa3a4dccc698033f0cbb7b8d2cb00b47c2c2d137
blaire <blaire.e.porter@gmail.com>
2021-03-26 08:04:41 -0400

    updated files

--- js/script.js
+++ js/script.js
@@ -23,35 +23,6 @@ themeSwitch.addEventListener('change', () => {
 // *********DARK LIGHT MODE END*****
 
 
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var dots = document.getElementsByClassName("dot");
-//     if (n > slides.length) { slideIndex = 1 }
-//     if (n < 1) { slideIndex = slides.length }
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " active";
-// }
-
 
 const form = document.getElementById("fs-frm");
 form.reset();
@@ -60,7 +31,6 @@ form.reset();
 // ******show on scroll*****
 // Detect request animation frame
 var scroll = window.requestAnimationFrame ||
-    // IE Fallback
     function (callback) { window.setTimeout(callback, 1000 / 60) };
 var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
@@ -82,10 +52,6 @@ loop();
 
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
-    // special bonus for those using jQuery
-    if (typeof jQuery === "function" && el instanceof jQuery) {
-        el = el[0];
-    }
     var rect = el.getBoundingClientRect();
     return (
         (rect.top <= 0
